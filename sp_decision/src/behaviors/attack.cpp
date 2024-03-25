294f94aaf97b4b100ffb5e921db9392c7c20d410
lp <2200187107@qq.com>
2024-03-25 21:57:12 +0800

    调整策略,守补给区

--- sp_decision/src/behaviors/attack.cpp
+++ sp_decision/src/behaviors/attack.cpp
@@ -33,25 +33,23 @@ namespace sp_decision
                 blackboard_ptr_->action_status_ = Blackboard::Action_Lock::ATTACK;
                 return BehaviorState::SUCCESS;
             }
-            else if (blackboard_ptr_->stage_remain_time < 290 && blackboard_ptr_->enemy_number[0] == 1) // 假设对方有英雄则前往二号点
-            {
-                std::stringstream str;
-                str << "behavior: attack_2";
-                blackboard_ptr_->LogPub(str.str());
-                ROS_INFO("attack_2");
-                attack_point_2();
-                blackboard_ptr_->action_status_ = Blackboard::Action_Lock::ATTACK;
-                return BehaviorState::SUCCESS;
-            }
+            // else if (blackboard_ptr_->stage_remain_time < 290 && blackboard_ptr_->enemy_number[0] == 1) // 假设对方有英雄则前往二号点
+            // {
+            //     std::stringstream str;
+            //     str << "behavior: attack_2";
+            //     blackboard_ptr_->LogPub(str.str());
+            //     ROS_INFO("attack_2");
+            //     attack_point_2();
+            //     blackboard_ptr_->action_status_ = Blackboard::Action_Lock::ATTACK;
+            //     return BehaviorState::SUCCESS;
+            // }
             else
             {
                 std::stringstream str;
                 str << "behavior: attack_1";
                 blackboard_ptr_->LogPub(str.str());
                 ROS_INFO("attack_1");
-                ROS_INFO("attack_1");
                 attack_point_1();
-                log_exe_ptr_->info("behavior: attack");
                 blackboard_ptr_->action_status_ = Blackboard::Action_Lock::ATTACK;
                 return BehaviorState::SUCCESS;
             }
@@ -63,7 +61,7 @@ namespace sp_decision
     {
         if (chassis_exe_ptr_->FastMove(blackboard_ptr_->attack_pos[0].x, blackboard_ptr_->attack_pos[0].y) == 1)
         {
-            chassis_exe_ptr_->observe(-100, 0);
+            chassis_exe_ptr_->observe(-10, 10);
         }
     }
     void AttackBehavior::attack_point_2() // TODO：补充云台方向控制———————————占据启动区
@@ -75,7 +73,7 @@ namespace sp_decision
         else if (chassis_exe_ptr_->FastMove(blackboard_ptr_->attack_pos[1].x, blackboard_ptr_->attack_pos[1].y) == 1)
         {
             chassis_exe_ptr_->Stop();
-            chassis_exe_ptr_->observe(-80, 90);
+            chassis_exe_ptr_->observe(-180, 180);
         }
     }
     void AttackBehavior::attack_point_3() // TODO：补充云台方向控制———————————冲对面家,点1
